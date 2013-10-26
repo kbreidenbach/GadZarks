e15bdb30f26d69c4aea01515be20d9b937cc954e
Kevin E. Breidenbach <kevin.breidenbach@gmail.com>
2013-10-25 19:24:51 -0500

    initial commit

---
+++ target/generated-sources/r/me/breidenbach/BuildConfig.java
@@ -0,0 +1,8 @@
+/*___Generated_by_IDEA___*/
+
+/** Automatically generated file. DO NOT MODIFY */
+package me.breidenbach;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
