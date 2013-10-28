d102ed8fce129b17bfa4cda86251acbcf4bf083e
Kevin E. Breidenbach <kevin.breidenbach@gmail.com>
2013-10-28 13:19:52 -0500

    Fixed so that unavailable cells can be clicked, but no zark info is displayed

--- src/main/java/me/breidenbach/gadzarks/views/SettingsView.java
+++ src/main/java/me/breidenbach/gadzarks/views/SettingsView.java
@@ -1,9 +1,24 @@
 package me.breidenbach.gadzarks.views;
 
+import android.widget.RelativeLayout;
+
 /**
  * User: Kevin Breidenbach
  * Date: 10/27/13
  * Time: 12:01 PM
  */
 public class SettingsView {
+    private final RelativeLayout layout;
+
+    public SettingsView(RelativeLayout layout) {
+        this.layout = layout;
+    }
+
+    public void hideLayout() {
+        layout.setVisibility(RelativeLayout.GONE);
+    }
+
+    public void showLayout() {
+        layout.setVisibility(RelativeLayout.VISIBLE);
+    }
 }
